53a05a94d6007c899d945df96b396b621ef914d7
Nguyễn Mạnh Hùng <nmh@netcompany.com>
2023-06-13 10:19:53 +0700

    update cron job

--- supper-search/update.sh
+++ supper-search/update.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-docker stop $(docker ps -a -q) && docker system prune -a -f
-docker-compose -f docker-compose.yml --env-file ./env up -d --force-recreate
+docker stop $(docker ps -a -q) && docker system prune -a -f && docker-compose -f docker-compose.yml --env-file ./env up -d --force-recreate
