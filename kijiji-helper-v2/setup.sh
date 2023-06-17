2753eb0c79a4ed64cf9f0b12b06adac09b62b800
Nguyễn Mạnh Hùng <nmh@netcompany.com>
2023-06-17 15:25:09 +0700

    add kijiji version 2

---
+++ kijiji-helper-v2/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+read -p "Enter Username: " username
+echo "UserName: $username"
+echo "HostName: $username-PC"
+cp env-ba env
+sed -i "s/DUMB-PC/$username-PC/g" env 
+docker-compose -f docker-compose.yml --env-file env --compatibility -p "kijiji" up -d --build --force-recreate
+chmod +x /root/production-deploy/kijiji-helper-v2/update.sh
+crontab -l | { cat; echo "*/10 * * * * /root/production-deploy/kijiji-helper-v2/update.sh >> /tmp/cron_output "; } | crontab -
