02ec903164ff476ee6184a0e037bb7dfabbb44d8
Nguyễn Mạnh Hùng <nmh@netcompany.com>
2023-06-20 08:04:06 +0700

    update setup

--- kijiji-helper-v2/setup.sh
+++ kijiji-helper-v2/setup.sh
@@ -6,4 +6,4 @@ cp env-ba env
 sed -i "s/DUMB-PC/$username-PC/g" env 
 docker-compose -f docker-compose.yml --env-file env --compatibility -p "kijiji" up -d --build --force-recreate
 chmod +x /root/production-deploy/kijiji-helper-v2/update.sh
-crontab -l | { cat; echo "*/10 * * * * /root/production-deploy/kijiji-helper-v2/update.sh >> /tmp/cron_output "; } | crontab -
+crontab -l | { cat; echo "0 23 * * * /root/production-deploy/kijiji-helper-v2/update.sh >> /tmp/cron_output "; } | crontab -
