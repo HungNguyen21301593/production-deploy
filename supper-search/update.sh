9cdb88ecf32c385bb58324211127628bf7ae7a0d
Nguyễn Mạnh Hùng <nmh@netcompany.com>
2023-09-01 17:03:39 +0700

    latest changes

--- supper-search/update.sh
+++ supper-search/update.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-docker stop $(docker ps -a -q) && docker system prune -a -f
 cd /root/production-deploy/supper-search/ && docker-compose -f docker-compose.yml --env-file ./env up -d --force-recreate
