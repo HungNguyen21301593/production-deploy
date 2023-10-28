b873f00a41b63d85282552461317c78b72ff110c
Nguyễn Mạnh Hùng <nmh@netcompany.com>
2023-10-28 15:07:11 +0700

    update

--- supper-search/executors/reinstall.sh
+++
@@ -1,3 +0,0 @@
-docker-compose -f docker-compose.yml --env-file ./env down
-docker image rm hungnguyen991995/supper-search
-sh update.sh
--- supper-search/executors/update.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-cd /root/production-deploy/supper-search/executors && docker-compose -f docker-compose.yml --env-file ./env up -d --force-recreate
