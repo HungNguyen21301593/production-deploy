1b9bdb01166c723dd1225090f3f769ecdfbb50b5
Nguyễn Mạnh Hùng <nmh@netcompany.com>
2023-11-16 07:48:30 +0700

    upda

--- supper-search/reinstall.sh
+++ supper-search/reinstall.sh
@@ -1,1 +1,1 @@
-docker-compose -f docker-compose.yml --env-file ./env down && docker image rm hungnguyen991995/supper-search && sh update.sh
+docker-compose -f docker-compose.yml --env-file ./env down && docker image rm hungnguyen991995/supper-search && && docker image rm hungnguyen991995/autosearch:latest && sh update.sh
